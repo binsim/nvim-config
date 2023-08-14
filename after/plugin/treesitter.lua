e89b0153b0c36723e01ce4c18838cd91d5c7e789
binsim <jonasspitzhirn@gmail.com>
2023-08-14 19:01:46 +0200

    fixed error zls to zig

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "vimdoc", "javascript", "typescript", "css", "scss", "c", "lua", "vim", "help", "zls"},
+  ensure_installed = { "vimdoc", "javascript", "typescript", "css", "scss", "c", "lua", "vim", "help", "zig"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,

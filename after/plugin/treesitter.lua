fc684952f8d27abd2a2b5c87b463bab55543e66e
binsim <jonasspitzhirn@gmail.com>
2023-08-17 17:19:55 +0200

    Removed help because of error message

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "vimdoc", "javascript", "typescript", "css", "scss", "c", "lua", "vim", "help", "zig"},
+  ensure_installed = { "vimdoc", "javascript", "typescript", "css", "scss", "c", "lua", "vim", "zig"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
